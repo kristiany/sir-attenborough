8c2fa1ea09c420c2d525dfe6be3b8ddbfdacfa91
kristiany <kristian.y@gmail.com>
2017-04-30 17:44:08 +0200

    Send request to Google speech API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
   , logger = require('morgan')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/start.jade')
+  , Speech = require('@google-cloud/speech')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -10,6 +11,33 @@ app.get('/', function (req, res, next) {
   try {
     var html = template({ title: 'Home' })
     res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/recognize', function (req, res, next) {
+  try {
+    const speech = Speech({
+      projectId: 'I-dont-know',
+      keyFilename: 'keyfile.json'
+    });
+    const googleRequest = {
+      encoding: 'FLAC',
+      sampleRateHertz: 16000,
+      languageCode: 'sv-SE'
+    };
+    //console.log("Data: " + req.query.data);
+    speech.recognize({content: req.query.data}, googleRequest)
+      .then((results) => {
+        const result = results[0];
+        console.log("Transcription: ${result}")
+        res.send('{ "status": "got result" }');
+      }, (reason) => {
+        console.log(reason);
+        res.status(500).send('Error calling speech API: ' + reas);
+      });
+      res.send('{ "status": "ok" }');
   } catch (e) {
     next(e)
   }
