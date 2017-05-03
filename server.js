32c9767f2c689cb2b4c5f903d2ca49afb8b4baed
kristiany <kristian.y@gmail.com>
2017-05-03 08:23:12 +0200

    Fuck googles client and its auth-problems. I am not running on GC.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 var express = require('express')
   , logger = require('morgan')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/start.jade')
   , Speech = require('@google-cloud/speech')
+  , https = require('https')
+  , bodyParser = require('body-parser')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -16,28 +19,65 @@ app.get('/', function (req, res, next) {
   }
 })
 
-app.get('/recognize', function (req, res, next) {
+/*
+  Might consider a streaming to server approach instead https://subvisual.co/blog/posts/39-tutorial-html-audio-capture-streaming-to-node-js-no-browser-extensions
+*/
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/recognize', function (req, res, next) {
   try {
-    const speech = Speech({
-      projectId: 'I-dont-know',
-      keyFilename: 'keyfile.json'
-    });
-    const googleRequest = {
-      encoding: 'FLAC',
-      sampleRateHertz: 16000,
-      languageCode: 'sv-SE'
+    /*const speech = Speech({
+      //projectId: 'sir-attenborough',
+      key: process.env.GOOGLE_API_KEY
+    });*/
+    const config = {
+      encoding: 'LINEAR16',
+      sampleRateHertz: 44100,
+      languageCode: 'sv-SE',
+      maxAlternatives: 1
     };
     //console.log("Data: " + req.query.data);
-    speech.recognize({content: req.query.data}, googleRequest)
+    /*speech.recognize({content: req.query.data}, config)
       .then((results) => {
         const result = results[0];
         console.log("Transcription: ${result}")
         res.send('{ "status": "got result" }');
       }, (reason) => {
         console.log(reason);
-        res.status(500).send('Error calling speech API: ' + reas);
-      });
-      res.send('{ "status": "ok" }');
+        res.status(500).send('Error calling speech API: ' + reason);
+      });*/
+    const post = {
+      host: 'speech.googleapis.com',
+      port : 443,
+      path: '/v1/speech:recognize?key=' + process.env.GOOGLE_API_KEY,
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    };
+    var request = https.request(post, function(speechResponse) {
+      console.log('Status: ' + speechResponse.statusCode);
+      console.log('Headers: ' + JSON.stringify(speechResponse.headers));
+      speechResponse.setEncoding('utf8');
+      speechResponse.on('data', (answer) => {
+        console.log('Answer: ' + answer);
+        res.send(answer);
+      })
+    });
+    console.log('data', req.body);
+    console.log('data len', req.body.length);
+    //console.log(req.body.data);
+    request.write(JSON.stringify({
+      config: config,
+      audio: {
+        content: req.body.data.substring(req.body.data.indexOf(',') + 1)
+      }
+    }));
+    request.end()
+    request.on('error', (reason) => {
+      console.log(reason);
+      res.status(500).send('Error calling speech API: ' + reason);
+    });
   } catch (e) {
     next(e)
   }
